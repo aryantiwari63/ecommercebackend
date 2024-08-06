3a9ee51099a53f8cebf55d390160e578c1430bfb
aryan <aryan.tiwari@adglobal360.com>
2024-08-06 18:03:16 +0530

    backend code updated

--- Controllers/Usercontroller.js
+++ Controllers/Usercontroller.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require("bcryptjs");
 const dotenv = require("dotenv");
 const nodemailer = require('nodemailer');
-
+const Cart = require("../Models/Cart");
 dotenv.config(); 
 
 
@@ -50,76 +50,6 @@ exports.gethomeproducts =async(req, res) => {
 
 
 
-    // exports.checklogin = async(req,res) => {
-    //     // const {email,password} = req.body;
-    //     //  console.log(email);
-    //     //       try{
-    //     //         const user = await useraccountmodel.findOne({ email });
-             
-    //     //       console.log(user);
-    //     //        if(!user){
-    //     //         return res.status(404).json({message:"user not exist"});
-    //     //        }
-             
-
-
-    //     // //        console.log("user provide ",password);
-    //     // //          console.log("db password",user.password);
-    //     // // if (password.trim() !== user.password.trim()) {
-    //     // //     console.log("Password incorrect");
-    //     // //     return res.status(400).json({ message: "Incorrect password" });
-    //     // // }
-    //     //         // const isMatch = await user.matchPassword(password);
-              
-              
-              
-    //     //         const isMatch = await bcrypt.compare(password, user.password);
-    //     //         if (!isMatch) {
-    //     //           return res.status(400).json({ message: 'Invalid credentials' });
-    //     //         }
-    //     //       const token = jwt.sign({ email: user.email }, process.env.secret_key, { expiresIn: '1h' });
-              
-    //     //        console.log(token);
-    //     //        res.status(200).json({ message: 'Login successful', token });
-    //     //       }
-    //     //       catch(error){
-    //     //         res.status(500).json({ message: 'Server error', error });
-    //     //       }
-    //       const { email, password } = req.body;
-
-    // try {
-     
-    //   if (!email || !password) {
-    //     console.log('Missing email or password');
-    //     return res.status(400).json({ message: 'Email and password are required' });
-    //   }
-  
-    //   console.log('Received login request with:', { email, password });
-  
-    //   const user = await useraccountmodel.findOne({ email });
-      
-    //   if (!user) {
-    //     console.log('User not found');
-    //     return res.status(400).json({ message: 'Invalid email or password' });
-    //   }
-  
-    //   const isMatch = await bcrypt.compare(password, user.password);
-      
-    //   if (!isMatch) {
-    //     console.log('Incorrect password');
-    //     return res.status(400).json({ message: 'Invalid email or password' });
-    //   }
-    //   console.log("esv",process.env.SECRET_KEY);
-    //    const token = jwt.sign({ id: user._id,email: user.email }, process.env.secret_key, { expiresIn: '1h' });
-    
-    //   console.log('Login successful');
-    //   return res.json({ message: 'Login successful'},token);
-      
-    // } catch (error) {
-    //   console.error('Server error during login:', error);
-    //   return res.status(500).json({ message: 'Server error' });
-    // }
-    // };
     exports.checklogin = async (req, res) => {
       const { email, password } = req.body;
   
@@ -219,3 +149,174 @@ exports.gethomeproducts =async(req, res) => {
     res.send('Password has been reset');
   };
 
+exports.profiledata = async(req,res) => {
+       try{
+          const user = await useraccountmodel.findById(req.user.id).select('-password');
+           console.log(user);
+          res.json(user);
+        }
+catch(error){
+       res.status(500).json({message:'server error'});
+}
+};
+
+// exports.addtocart = async(req,res) => {
+//   const { productId, quantity } = req.body;
+  
+
+//    console.log('productId, quantity',productId, quantity);
+//   try {
+//     const cart = await Cart.findOne({ userId: req.user.id });
+
+//     if (cart) {
+//       // Cart exists for the user
+//       const itemIndex = cart.items.findIndex(item => item.productId == productId);
+
+//       if (itemIndex > -1) {
+//         // Product exists in the cart, update the quantity
+//         cart.items[itemIndex].quantity += quantity;
+//       } else {
+//         // Product does not exist in cart, add new item
+//         cart.items.push({ productId, quantity,price });
+//       }
+
+//       await cart.save();
+//       res.status(200).json(cart);
+//     } else {
+//       // No cart for user, create new cart
+//       const newCart = new Cart({
+//         userId: req.user.id,
+//         items: [{ productId, quantity }]
+//       });
+
+//       await newCart.save();
+//       console.log("new card complete")
+//       res.status(200).json(newCart);
+//     }
+//   } catch (error) {
+//     res.status(500).json({ message: 'Server error' });
+//   }
+// };
+exports.addtocart = async(req, res) => {
+  const { productId, quantity } = req.body;
+
+  try {
+    const product = await Datamodel.findById(productId); // Assuming you are using _id for finding products
+    if (!product) {
+      return res.status(404).json({ message: 'Product not found' });
+    }
+    
+    const { price, name } = product;
+    console.log(price,name);
+    const cart = await Cart.findOne({ userId: req.user.id });
+
+    if (cart) {
+      const itemIndex = cart.items.findIndex(item => item.productId.toString() === productId);
+      if (itemIndex > -1) {
+        cart.items[itemIndex].quantity += quantity;
+      } else {
+        cart.items.push({ productId, quantity, price, name });
+      }
+
+      await cart.save();
+      res.status(200).json(cart);
+    } else {
+      const newCart = new Cart({
+        userId: req.user.id,
+        items: [{ productId, quantity, price, name }]
+      });
+
+      await newCart.save();
+      res.status(200).json(newCart);
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'Server error' });
+  }
+};
+
+exports.usercart = async(req,res) =>{
+  try {
+      console.log(req.user.id);
+    const cart = await Cart.findOne({ userId: req.user.id });  //.populate('items.productId'); Populate product details
+    if (!cart) {
+      return res.status(404).json({ message: 'Cart not found' });
+    }
+    console.log(cart);
+    res.status(200).json(cart);
+  } catch (error) {
+    res.status(500).json({ message: 'Server error' });
+  }
+};
+
+
+exports.increaseQuantity = async (req, res) => {
+  const { productId } = req.body;
+
+  try {
+    const cart = await Cart.findOne({ userId: req.user.id });
+    if (!cart) {
+      return res.status(404).json({ message: 'Cart not found' });
+    }
+
+    const itemIndex = cart.items.findIndex(item => item.productId.toString() === productId);
+    if (itemIndex >= 0) {//0 because if not find then return -1 so we can check >-1 or >=0
+      cart.items[itemIndex].quantity += 1;
+      await cart.save();
+      return res.status(200).json(cart);
+    } else {
+      return res.status(404).json({ message: 'Item not found in cart' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'Server error' });
+  }
+};
+
+exports.decreaseQuantity = async (req, res) => {
+  const { productId } = req.body;
+  console.log(productId,"work");
+  try {
+    const cart = await Cart.findOne({ userId: req.user.id });
+    if (!cart) {
+      return res.status(404).json({ message: 'Cart not found' });
+    }
+
+    const itemIndex = cart.items.findIndex(item => item.productId.toString() === productId);
+    if (itemIndex >= 0) {
+      if (cart.items[itemIndex].quantity > 0) {
+        cart.items[itemIndex].quantity -= 1;
+      }
+      await cart.save();
+      return res.status(200).json(cart);
+    } else {
+      return res.status(404).json({ message: 'Item not found in cart' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'Server error' });
+  }
+};
+
+exports.deletecartproduct = async (req, res) => {
+  const { id } = req.params;
+  try {
+    console.log('id is',id);
+     
+    const result = await Datamodel.findByIdAndDelete(id);
+    console.log(result); 
+    if (result) {
+      res.json({ message: 'Profile deleted successfully!' });
+    } else {
+      res.status(404).json({ message: 'Profile not found' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'Failed to delete profile', error: error.message });
+  }
+};
+
+exports.logout = async (req, res) => {
+  try {
+   
+    return res.status(200).json('Logged out successfully');
+  } catch (error) {
+    return res.status(500).json('Error logging out');
+  }
+};
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,58 @@
 const express = require("express");
 const router = express.Router();
 const bodyparser = require("body-parser");
-// const Controller = require("../Controllers");
+
 const Datamodel = require("../Models/Data");
 const useraccountmodel = require("../Models/Useraccount");
 const jwt = require('jsonwebtoken');
 const bcrypt = require("bcryptjs");
 const dotenv = require("dotenv");
 const nodemailer = require('nodemailer');
-const Usersontroller = require("../Controllers/Usercontroller")
+const Usersontroller = require("../Controllers/Usercontroller");
+
+
+// const authmiddleware = (req,res,next) => {
+//         const authheader = req.header('Authorization');
+//         console.log(authheader);
+//         if(!authheader){
+//             return res.status(401).json({message:'no token'});
+//         }
+//         try{
+//             const decoded = jwt.verify(authheader,process.env.SECRET_KEY);
+//              console.log("decoded",decoded);
+//              req.user=decoded;
+//              next();
+//         }
+//         catch(error){
+//             return res.status(401).json({message:'token is not valid'});
+//         }
+// }
+const authmiddleware = (req, res, next) => {
+    const authheader = req.header('Authorization');
+    console.log('Authorization Header:', authheader);
+    if (!authheader) {
+      return res.status(401).json({ message: 'no token' });
+    }
+  
+    // Extract the token part after 'Bearer '
+    // const token = authheader.replace('Bearer ', '');
+    const token= authheader;
+    console.log('Token:', token);
+  
+    try {
+      const decoded = jwt.verify(token, process.env.SECRET_KEY);
+      console.log('Decoded:', decoded);
+      req.user = decoded;
+      next();
+    } catch (error) {
+      console.log('Token Verification Error:', error);
+      return res.status(401).json({ message: 'token is not valid' });
+    }
+  };
+  
+  module.exports = authmiddleware;
+  
+
 
 router.get('/products/:category', Usersontroller.gethomeproducts);
   
@@ -19,27 +63,21 @@ router.post('/login',Usersontroller.checklogin );
 router.post('/forgotpasswordrequest',Usersontroller.changepasswordrequest);
 
  router.post('/reset-password/:token',Usersontroller.changepassword);
-// router.post('/reset-password/:token', async (req, res) => {
-//   const { token } = req.params;
-//   const { newpassword } = req.body;
-//   console.log(newpassword);
-//   let decoded;
-//   try {
-//     decoded = jwt.verify(token,process.env.SECRET_KEY);
-//   } catch (err) {
-//     return res.status(400).send('Invalid or expired token');
-//   }
 
-//   const user = await useraccountmodel.findById(decoded.id);
-//   if (!user) {
-//     return res.status(404).send('User not found');
-//   }
 
-//   const salt = await bcrypt.genSalt(10);
-//   user.password = await bcrypt.hash(password, salt);
-//   await user.save();
-//     console.log("password saved");
-//   res.send('Password has been reset');
-// });
+ router.get('/profiledata',authmiddleware,Usersontroller.profiledata);
+
+ router.post('/addtocart',authmiddleware,Usersontroller.addtocart);
+
+ router.get('/usercart',authmiddleware,Usersontroller.usercart);
+
+ 
+ router.post('/incquantity',authmiddleware,Usersontroller.increaseQuantity);
+
+ router.post('/decquantity',authmiddleware,Usersontroller.decreaseQuantity);
+ 
+router.delete('/deletecartproduct/:id',Usersontroller.deletecartproduct);
+
+ router.post('/logout', Usersontroller.logout);
 
   module.exports = router;
